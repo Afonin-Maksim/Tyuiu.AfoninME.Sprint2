4387ae77cde7b4550585c03fe499dcc9d70968b8
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-10-17 18:54:40 +0500

    Task4

---
+++ Tyuiu.AfoninME.Sprint1.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AfoninME.Sprint2.Task4.V10.Lib;
+
+namespace Tyuiu.AfoninME.Sprint2.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Test_WhenConditionTrue()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 10;
+            double expected = Math.Round(Math.Pow(1 + (3 / y), x), 3);
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test_WhenConditionFalse()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 2;
+            double expected = Math.Round(y + 10 * x - (5 / x), 3);
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.AfoninME.Sprint1.Task4.V10/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.AfoninME.Sprint2.Task4.V10.Lib;
+
+namespace Tyuiu.AfoninME.Sprint2.Task4.V10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Афонин М. Е. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Афонин Максим Евгеньевич | ИСТНб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение функции Z с              *");
+            Console.WriteLine("* использованием тернарного оператора. Округлить результат до 3 знаков.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double z = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Z = {z}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Программа завершена                                                     *");
+            Console.WriteLine("***************************************************************************");
+        }
+    }
+}
