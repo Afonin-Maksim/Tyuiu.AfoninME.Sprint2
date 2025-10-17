620d2d9c00952ffb93eca7d2f8caba67a0675d6c
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-10-17 18:31:38 +0500

    Task3

---
+++ Tyuiu.AfoninME.Sprint2.Task3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.AfoninME.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.AfoninME.Sprint2.Task3.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Test_XGreaterThanOne()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(2);
+            double expected = Math.Round(2 * Math.Pow((2 + 1) / (2 - 1), 2), 3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test_XEqualsZero()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(0);
+            double expected = Math.Round(Math.Pow(0, 2) + Math.Cos(Math.Pow(0, 2)) - 17, 3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test_XBetweenMinus27AndTwo()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(-5);
+            double expected = Math.Round(Math.Pow(-5, 2) - Math.Sin(Math.Pow(-5, 2)) + 3, 3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test_XLessOrEqualMinus27()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(-30);
+            double expected = Math.Round(-30 + 10 * (-30) - (1 / Math.Pow(-30, 2)), 3);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.AfoninME.Sprint2.Task3.V23/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AfoninME.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.AfoninME.Sprint2.Task3.V23
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Афонин М. Е. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Афонин Максим Евгеньевич | ИСТНб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение функции Y с              *");
+            Console.WriteLine("* использованием вложенных операторов if-else. Округлить результат        *");
+            Console.WriteLine("* до трёх знаков после запятой.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            double y = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Y = {y}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Программа завершена                                                     *");
+            Console.WriteLine("***************************************************************************");
+        }
+    }
+}
